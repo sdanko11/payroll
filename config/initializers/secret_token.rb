4066ef13decb7933eaa3d7ab048a98e5c26cc6fb
Dan Pickett <dan.pickett@launchware.com>
2013-12-07 16:24:31 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Payroll::Application.config.secret_key_base = '3365b9dc32f8f7046e6e3db842290e26348a41eff8cac41b83f843d8c4e3538d7c2ce02d9cf2cee431e41ffd95c7d02486f7632eb1bb2ba429e590ddd91bd7c3'
